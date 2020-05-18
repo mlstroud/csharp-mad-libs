97ed217ac9ec7d5492c4ff8f2670d7da9e069500
Matt Stroud <lixingke06@gmail.com>
2020-05-18 11:47:31 -0700

    work on unicorn story

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -24,5 +24,24 @@ namespace MadLibs.Controllers
       Game pirate = new Game(new string[] { noun }, new string[] { verb }, new string[] { adjective }, new string[] { pluralnoun });
       return View(pirate);
     }
+
+    [Route("/unicorns")]
+    public ActionResult Unicorns()
+    {
+      return View();
+    }
+
+    [Route("/unicorns/story")]
+    public ActionResult UnicornStory(string pluralnoun, string adjective, string pluralnoun2, string pluralnoun3, string adjective2, string adjective3,
+      string adjective4, string noun, string pluralnoun4, string adjective5, string verb, string pluralnoun5, string verb2, string verb3,
+      string noun2, string adjective6)
+    {
+      string[] nouns = new string[] { noun, noun2 };
+      string[] verbs = new string[] { verb, verb2, verb3 };
+      string[] adjectives = new string[] { adjective, adjective2, adjective3, adjective4, adjective5, adjective6 };
+      string[] pluralNouns = new string[] { pluralnoun, pluralnoun2, pluralnoun3, pluralnoun4, pluralnoun5 };
+      Game unicorn = new Game(nouns, verbs, adjectives, pluralNouns);
+      return View(unicorn);
+    }
   }
 }
